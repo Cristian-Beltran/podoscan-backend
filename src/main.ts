8879652ac38cfaf6cfe2630a2e3fefe33ba00f7b
cristian <cristian.beltran.paco@gmail.com>
2025-10-31 19:25:34 -0400

    finish

--- src/main.ts
+++ src/main.ts
@@ -8,33 +8,41 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Archivos estáticos
   app.use('/img', express.static(join(process.cwd(), 'src', 'context', 'img')));
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('Documentación de la API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  // ⚠️ Ajusta HOSTNAME a tu mDNS real (ej. "devpc.local")
+
   app.enableCors({
     origin: true,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Disposition'],
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
+      transformOptions: { enableImplicitConversion: true },
     }),
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(3000);
+
+  // 🔑 Clave: bind a 0.0.0.0
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
